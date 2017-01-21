e60b735486525c6bf1e69ab7e56747ad70c2680e
Volkmar Volli Schlothauer <GHSVS-de@users.noreply.github.com>
2017-01-21 23:42:39 +0100

    Initial creation

---
+++ autousergroupbyemail.php
@@ -0,0 +1,58 @@
+<?php
+defined('_JEXEC') or die;
+
+class PlgUserAutoUserGroupByEmail extends JPlugin
+{
+
+ protected $app;
+
+ public function onUserAfterSave($user, $isnew, $success, $msg)
+ {
+  
+  // Nur neue User. Nur Registrierungen im Frontend.
+  if ($isnew && $this->app->isSite())
+  {
+
+   /*
+   #__usergroups (kann bei jedem Joomla abweichen! Selber in DB schauen!):
+   1:Public
+   2:Registered
+   3:Author
+   4:Editor
+   5:Publisher
+   6:Manager
+   7:Administrator
+   8:Super Users
+   10:Shop Suppliers (Example)
+   12:Customer Group (Example)
+   13:Guest
+   */
+
+   $groups = array(
+    'firma1.com' => array(5),
+    'firma2.com' => array(6, 10),
+    'ghsvs.de' =>  array(1, 2, 3, 4, 5, 6, 7, 10, 12, 13),
+    'default' => array(2),
+   );
+   
+   $userNew = JFactory::getUser($user['id']);
+   
+   $parts = explode('@', $userNew->get('email'));
+   
+   if (!empty($parts[1]) && isset($groups[$parts[1]]))
+   {
+    $groups = $groups[$parts[1]];
+   }
+   else
+   {
+    $groups = $groups['default'];
+   }
+   
+   $userNew->set('groups', $groups);   
+   
+   $userNew->save();
+   
+  }
+ }
+
+}
